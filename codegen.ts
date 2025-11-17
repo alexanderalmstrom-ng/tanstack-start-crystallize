49e2825b4eeb030510d3a8d84da6f3462e70c0b4
Alexander Almström <info@alexanderalmstrom.com>
2025-11-17 16:51:45 +0100

    Update codegen.ts to use import.meta.env for environment variables instead of process.env

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,8 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 import z from 'zod'
 
-const SHOPIFY_SHOP_NAME = z.string().min(1, "SHOPIFY_STORE_NAME is required").parse(process.env.SHOPIFY_SHOP_NAME)
-const SHOPIFY_ACCESS_TOKEN = z.string().min(1, "SHOPIFY_STORE_PRIVATE_TOKEN is required").parse(process.env.SHOPIFY_ACCESS_TOKEN)
+const SHOPIFY_SHOP_NAME = z.string().min(1, "SHOPIFY_SHOP_NAME is required").parse(import.meta.env.SHOPIFY_SHOP_NAME)
+const SHOPIFY_ACCESS_TOKEN = z.string().min(1, "SHOPIFY_ACCESS_TOKEN is required").parse(import.meta.env.SHOPIFY_ACCESS_TOKEN)
  
 const config: CodegenConfig = {
   schema: [
