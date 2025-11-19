2d08848e8be506570550f6e35c04c413b1edece2
Alexander Almström <info@alexanderalmstrom.com>
2025-11-19 10:33:13 +0100

    Fix fragment masking of images on pdp

--- src/routes/product.$slug.tsx
+++ src/routes/product.$slug.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
-import { getProductBySlugServerFn } from "@/utils/product";
+import { createServerFn } from "@tanstack/react-start";
+import z from "zod";
+import { getFragmentData, graphql } from "@/gql";
+import type { ProductBySlugQuery } from "@/gql/graphql";
+import shopifyClient from "@/services/shopify/client";
 
 export const Route = createFileRoute("/product/$slug")({
   component: RouteComponent,
@@ -18,22 +22,68 @@ export const Route = createFileRoute("/product/$slug")({
 
 function RouteComponent() {
   const { product } = Route.useLoaderData();
+  const images = resolveProductImages(product);
 
   return (
     <div>
       <h1>{product?.title}</h1>
-      {product?.media.nodes.map((node) => {
-        if (node.__typename === "MediaImage") {
-          return (
+      {images?.map(
+        (image) =>
+          image?.image?.url && (
             <img
-              key={node.id}
-              src={node.image?.url}
-              alt={node.image?.altText ?? ""}
+              key={image.id}
+              src={image.image.url}
+              alt={image.image.altText ?? ""}
+              width={image.image.width ?? 2000}
+              height={image.image.height ?? 2000}
+              className="w-full h-full object-fit mix-blend-multiply"
             />
-          );
-        }
-        return null;
-      })}
+          ),
+      )}
     </div>
   );
 }
+
+const mediaImageFragment = graphql(`
+  fragment mediaImage on MediaImage {
+    __typename
+    id
+    image {
+      url
+      altText
+      width
+      height
+    }
+  }
+`);
+
+const productBySlugQuery = graphql(`
+  query ProductBySlug($slug: String!) {
+    product(handle: $slug) {
+      id
+      title
+      description
+      media(first: 1) {
+        nodes {
+          ...mediaImage
+        }
+      }
+    }
+  }
+`);
+
+async function getProductBySlug(slug: string) {
+  return (await shopifyClient(productBySlugQuery, { slug })).data?.product;
+}
+
+const getProductBySlugServerFn = createServerFn()
+  .inputValidator(z.object({ slug: z.string() }))
+  .handler(({ data: { slug } }) => getProductBySlug(slug));
+
+const resolveProductImages = (product: ProductBySlugQuery["product"]) => {
+  return product?.media?.nodes.map((node) =>
+    node.__typename === "MediaImage"
+      ? getFragmentData(mediaImageFragment, node)
+      : undefined,
+  );
+};
--- src/utils/product.ts
+++ src/utils/product.ts
@@ -31,55 +31,11 @@ export const productsQuery = graphql(`
   }
 `);
 
-export const mediaImageFragment = graphql(`
-  fragment mediaImage on MediaImage {
-    __typename
-    image {
-      url
-      altText
-      width
-      height
-    }
-  }
-`);
-
-export const productBySlugQuery = graphql(`
-  query ProductBySlug($slug: String!) {
-    product(handle: $slug) {
-      title
-      handle
-      media(first: 10) {
-        nodes {
-          __typename
-          id
-          ... on MediaImage {
-            image {
-              url
-              altText
-              width
-              height
-            }
-          }
-        }
-      }
-    }
-  }
-`);
-
 export async function getProducts() {
   const { data } = await shopifyClient(productsQuery);
   return data?.products?.edges?.map((edge) => edge.node);
 }
 
-export async function getProductBySlug(slug: string) {
-  const { data } = await shopifyClient(productBySlugQuery, { slug });
-  return data?.product;
-}
-
 export const getProductsServerFn = createServerFn().handler(() =>
   getProducts(),
 );
-
-export const getProductBySlugServerFn = createServerFn()
-  .inputValidator(z.object({ slug: z.string() }))
-  .handler(({ data: { slug } }) => getProductBySlug(slug));
