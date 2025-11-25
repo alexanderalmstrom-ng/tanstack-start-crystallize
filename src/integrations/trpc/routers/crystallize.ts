cbd5a5e656583f74aff6dc256841f55d0b5fe1e1
Alexander Almström <info@alexanderalmstrom.com>
2025-11-25 10:55:39 +0100

    Add GraphQL configuration for Crystallize API and update VSCode settings for TypeScript support. Enhance product retrieval in TRPC router to fetch data from Crystallize API.

--- src/integrations/trpc/routers/crystallize.ts
+++ src/integrations/trpc/routers/crystallize.ts
@@ -1,7 +1,25 @@
+import { graphql } from "@/gql";
+import { crystallizeApiClient } from "@/integrations/crystallize/client";
 import { createTRPCRouter, publicProcedure } from "../init";
 
 export const crystallizeRouter = createTRPCRouter({
   products: publicProcedure.query(() => {
-    return undefined;
+    return crystallizeApiClient({
+      endpoint: "catalogue",
+      query: graphql(`
+        query Products {
+          catalogue {
+            subtree(type: product) {
+              edges {
+                node {
+                  id
+                  path
+                }
+              }
+            }
+          }
+        }
+      `),
+    });
   }),
 });
