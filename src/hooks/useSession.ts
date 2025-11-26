bbd9fc7824749cbe12185684d5df8d27665e9b27
Alexander Almström <info@alexanderalmstrom.com>
2025-11-27 00:49:29 +0100

    Remove redundant session max age

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -9,7 +9,6 @@ type SessionData = {
 export function useSession() {
   return TanstackServer.useSession<SessionData>({
     name: "session",
-    maxAge: 2592000, // 30 days
     password: z.string().min(32).parse(process.env.SESSION_SECRET), // At least 32 characters
     cookie: {
       secure: process.env.NODE_ENV === "production",
